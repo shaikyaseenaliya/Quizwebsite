5451bd40954e9e18e47063b1ce4e9646c7fd2577
shaikyaseenaliya <157492467+shaikyaseenaliya@users.noreply.github.com>
2024-02-22 16:12:52 +0530

    Add files via upload

---
+++ syncTask22.js
@@ -0,0 +1,61 @@
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Type Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Cohedal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Preprocessor",
+            "C. Hypertext Programming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Structured Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML stands for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Markup Language", 
+            "B. ExploreMarkup Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+];
